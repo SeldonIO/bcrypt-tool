c050566f8cdf14f30e2010c517358275cd5a2d9f
Gurminder Sunner <gsunner2000@gmail.com>
2019-11-06 10:29:06 +0000

    simple hardcoded password hash generation

--- cmd/bcrypt-tool.go
+++ cmd/bcrypt-tool.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func main() {
-	fmt.Println("ok")
+	var clearPassword = []byte("testpassword")
+	cost := bcrypt.DefaultCost
+	hashedPassword, err := bcrypt.GenerateFromPassword(clearPassword, cost)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	} else {
+		fmt.Printf("%v", string(hashedPassword))
+	}
 }
