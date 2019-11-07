dd7b7d31565969ce8843e619b3911f023e562c60
Gurminder Sunner <gsunner2000@gmail.com>
2019-11-07 17:06:58 +0000

    add version and command line args support

--- cmd/bcrypt-tool.go
+++ cmd/bcrypt-tool.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -9,16 +10,39 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	rawInputText, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
-	}
-	rawInputText = strings.TrimSpace(rawInputText)
+var Version string = "unkown"
 
-	var clearPassword = []byte(rawInputText)
+type cliFlags struct {
+	version *bool // value of -version
+}
+
+type cliArgs struct {
+	clearPassword *string // value of arg 1
+}
+
+func main() {
+	cli_flags := getCliFlags()
+	cli_args := getCliArgs()
+
+	if *cli_flags.version { // check -version flag
+		printVersionAndExit()
+	}
+
+	var clearPassword []byte
+	if cli_args.clearPassword != nil {
+		clearPassword = []byte(*cli_args.clearPassword)
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		rawInputText, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+		rawInputText = strings.TrimSpace(rawInputText)
+
+		clearPassword = []byte(rawInputText)
+	}
+
 	cost := bcrypt.DefaultCost
 	hashedPassword, err := bcrypt.GenerateFromPassword(clearPassword, cost)
 	if err != nil {
@@ -26,4 +50,30 @@ func main() {
 		os.Exit(2)
 	}
 	fmt.Printf("%v", string(hashedPassword))
+}
+
+func printVersionAndExit() {
+	fmt.Println(Version)
+	os.Exit(0)
+}
+
+func getCliFlags() cliFlags {
+	cli_flags := cliFlags{}
+
+	cli_flags.version = flag.Bool("version", false, "Show version.")
+	flag.Parse()
+
+	return cli_flags
+}
+
+func getCliArgs() cliArgs {
+	cli_args := cliArgs{}
+
+	if len(os.Args) > 1 { // is there a password arg
+		cli_args.clearPassword = &os.Args[1]
+	} else {
+		cli_args.clearPassword = nil
+	}
+
+	return cli_args
 }
